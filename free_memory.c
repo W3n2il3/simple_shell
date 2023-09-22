5bba440ff0bf52e0ee7b391467326f28d47e0555
bathobile <pearlbathobile@gmail.com>
2023-09-21 18:32:47 -0700

    shell

--- free_memory.c
+++ free_memory.c
@@ -10,11 +10,11 @@ void free_memory(char **args)
 {
 	/*size_t i;*/
 
-	
-	for (i = 0; args[i] != NULL; i++)
-	{
-		free(args[i]);
-	}
-	
+	/**
+	* for (i = 0; args[i] != NULL; i++)
+	*{
+	*	free(args[i]);
+	*}
+	*/
 	free(args);
 }
